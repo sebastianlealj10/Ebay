b36f738869fb64f0051a85adeeaf0a796b71e8c4
Sebastian <sebastianlealj10@gmail.com>
2019-07-31 07:31:17 -0500

    sys.path added

--- Automation/Scripts/Search.py
+++ Automation/Scripts/Search.py
@@ -1,5 +1,16 @@
 import unittest
 import time
+import sys
+
+sys.path.extend(['/home/sebas/PycharmProjects/Ebay', '/home/sebas/PycharmProjects/Ebay/Automation', '/home/sebas'
+                                                                                                    '/PycharmProjects'
+                                                                                                    '/Ebay/Automation'
+                                                                                                    '/PageObjects',
+                 '/home/sebas/PycharmProjects/Ebay/Automation/Scripts',
+                 '/home/sebas/PycharmProjects/Ebay/Automation/PageObjects/Pages',
+                 '/home/sebas/PycharmProjects/Ebay/venv', '/home/sebas/PycharmProjects/Ebay/venv/bin',
+                 '/home/sebas/PycharmProjects/Ebay/venv/include', '/home/sebas/PycharmProjects/Ebay/venv/lib',
+                 '/home/sebas/PycharmProjects/Ebay/venv/lib64'])
 
 from selenium import webdriver
 from Pages import Homepage
@@ -7,6 +18,10 @@ from Pages import Searchpage
 from functions import sort_by_price_asc
 from functions import sort_by_price_desc
 from functions import sort_by_name_desc
+
+import sys;
+
+print('Python %s on %s' % (sys.version, sys.platform))
 
 
 class SearchShoes(unittest.TestCase):
@@ -34,7 +49,6 @@ class SearchShoes(unittest.TestCase):
         time.sleep(5)
         items = search_page.takeproducts(number_of_items)
         print("First 5 items found: " + str(items))
-        print(len(items))
         print("Items sorted by name ASC" + str(sort_by_price_desc(items)))
         print("Items sorted by price DESC" + str(sort_by_name_desc(items)))
         self.assertListEqual(items, sort_by_price_asc(items),
